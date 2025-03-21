7627ca5cbad4849dd7ef1dea80da7075ea65a1ec
qamar serhan <serhanqamar@gmail.com>
2025-03-21 14:54:01 +0200

    add cors

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -38,9 +38,17 @@ exports.verifyToken = (req, res, next) => {
 };
 
 // Ensures the user has admin privileges 
-exports.isAdmin = (req, res, next) => {
-    if (req.user.role !== 'admin') {
-        return res.status(403).json({ message: 'Access denied. Admins only.' });
-    }
+exports.isAdmin = ( req, res, next ) =>
+{
+    console.log(req.user);
+      if (!req.user) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized. No user found." });
+      }
+
+      if (req.user.role !== "admin") {
+        return res.status(403).json({ message: "Access denied. Admins only." });
+      }
     next(); // Proceeds to the next middleware or route handler.
 };
